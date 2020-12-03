6af2021c83d02b185a77c1d9f3dd221ca400af62
Patrick Piotrowski <pat_pi@yahoo.com>
2020-12-03 21:17:29 +0100

    add db configuration for sqllite

--- python_recap/__init__.py
+++ python_recap/__init__.py
@@ -1,11 +1,23 @@
+import os
+
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 def create_app():
     python_recap = Flask(__name__)
 
-    from python_recap.views import bp_main
+    python_recap.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'python_recap.db')}"
+    python_recap.config['SECRET_KEY'] = 'pint'
 
+    from python_recap.views import bp_main
     python_recap.register_blueprint(bp_main)
+
+    db.init_app(python_recap)
+    Migrate(python_recap, db)
 
     return python_recap
